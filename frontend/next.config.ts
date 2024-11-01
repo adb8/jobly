a72b98ff9be54a1c1769158fefe8c7b233e588d3
adb8 <abuahmed0821@gmail.com>
2024-11-01 01:31:57 -0400

    api changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,17 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
-  },
-};
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+        },
+      ],
+    },
+}
+
+module.exports = nextConfig
